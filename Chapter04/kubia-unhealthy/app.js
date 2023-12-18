2bcdc598e49058e7f7155da43a8761cec9414ff0
hk <hk@yl.com.cn>
2023-12-18 14:16:23 +0800

    try unhealthy probe pod.

--- Chapter04/kubia-unhealthy/app.js
+++ Chapter04/kubia-unhealthy/app.js
@@ -8,13 +8,14 @@ var requestCount = 0;
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   requestCount++;
-  if (requestCount > 5) {
+  if (requestCount > 5) {  // 服务器响应超过5次后将返回500错误
     response.writeHead(500);
     response.end("I'm not well. Please restart me!");
     return;
   }
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("You've hit " + os.hostname() + ", " 
+	+ requestCount + " times.\n");
 };
 
 var www = http.createServer(handler);
